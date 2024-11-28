c880b778b47374db787b970c69e4dda68f6fa641
CoderUzumaki <abhinav.mishra.x.viii@gmail.com>
2024-11-29 04:31:02 +0530

    BIG CHANGE - Deleting all files and restarting the project with a newly found energy and idea.

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as authController from '../controllers/auth.controller.js';
-const router = express.Router();
-
-
-router.post('/signup', authController.signup);
-router.post('/', authController.login);
-
-export default router;
