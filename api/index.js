495c87bb90e5572e47c4e521426ddf0547c0dce9
CoderUzumaki <abhinav.mishra.x.viii@gmail.com>
2024-11-30 14:19:15 +0530

    created a test api route

--- api/index.js
+++ api/index.js
@@ -1,16 +1,26 @@
 import express from 'express';
 import mongoose, { mongo } from 'mongoose';
+
+
+// import Routes
+import userRouter from './routes/user.router.js';
+
+// import dotenv
 import dotenv from 'dotenv';
 dotenv.config();
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO).then(() => { 
     console.log('Connected to MongoDB!');
 }).catch(err => {
     console.log('Failed to connect to MongoDB!', err);
 });
 
+// Create an Express App
 const app = express();
-
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}!`);
 });
+
+// Middleware
+app.use('/user/api', userRouter);
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,39 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
-  return (
-    <header className="py-4 bg-slate-300 shadow-md">
-      <div className="container flex justify-between items-center max-w-6xl mx-auto px-3">
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="py-4 bg-sky-200 shadow-md">
+      <div className="container flex justify-between items-center max-w-6xl mx-auto px-3">
         {/* Logo Section */}
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-700">Wander</span>
-            <span className="text-slate-500">ly.</span>
+          <h1 className="font-bold text-xl sm:text-xl flex flex-wrap items-center justify-center">
+            <span className="text-slate-900 hover:text-teal-600">Wanderly</span>
+            <span className="text-teal-700 text-3xl">.</span>
           </h1>
         </Link>
 
         {/* Navigation Section */}
         <div>
-          <ul className="flex gap-4">
+        <ul
+            className={`${
+              isOpen ? "absolute top-16 left-0 w-full gap-8 bg-sky-200 shadow-lg p-4 z-10" : "hidden"
+            } sm:flex gap-4 text-indigo-900 font-semibold`}
+          >
             <Link to="/">
-              <li className="sm-inline hover:underline">Home</li>
+              <li className="hover:underline hover:text-indigo-700">Home</li>
             </Link>
             <Link to="/about">
-              <li className="sm-inline hover:underline">About</li>
+              <li className="hover:underline hover:text-indigo-700">About</li>
             </Link>
             <Link to="/trips">
-              <li className="sm-inline hover:underline">Trips</li>
+              <li className="hover:underline hover:text-indigo-700">Trips</li>
             </Link>
             <Link to="/gallery">
-              <li className="sm-inline hover:underline">Gallery</li>
+              <li className="hover:underline hover:text-indigo-700">Gallery</li>
             </Link>
           </ul>
         </div>
 
         {/* User Section */}
+        <div className="flex gap-4 ">
 
+          {/* Nav Toggle */}
+          <button onClick={toggle}>
+            {isOpen ? (
+              // Close Icon
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              // Hamburger Icon
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6 sm:hidden text-slate-900"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            )}
+          </button>
+
+          {/* user */}
+          <Link to={"/login"}  className="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 text-slate-900"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+          </Link>
+        </div>
       </div>
     </header>
   );
